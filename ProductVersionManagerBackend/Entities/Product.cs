704c6eb6fa619fc9de1d6fd3a91a884e71b0fc1f
NeshGogo <rafaelaguero10@hotmail.com>
2021-09-07 18:22:49 -0400

    init backend

---
+++ ProductVersionManagerBackend/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductVersionManagerBackend.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Brand { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [StringLength(30)]
+        public string Seller { get; set; }
+        public Boolean Deleted { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        public DateTime ModificationDate { get; set; }
+        public List<ProductVersion> ProductVersions { get; set; }
+    }
+}
---
+++ ProductVersionManagerBackend/Entities/ProductVersion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductVersionManagerBackend.Entities
+{
+    public class ProductVersion
+    {
+        public int ProductVersionId { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Brand { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [StringLength(30)]
+        public string Seller { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public DateTime ModificationDate { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+
+    }
+}
