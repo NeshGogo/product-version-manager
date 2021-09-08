21c5339e9d1169da247f583d040c1ae0926e72c8
NeshGogo <rafaelaguero10@hotmail.com>
2021-09-08 13:42:13 -0400

    product form component

--- product-version-maneger-frontend/src/app/app-routing.module.ts
+++ product-version-maneger-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products/create',
+    component: ProductFormComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- product-version-maneger-frontend/src/app/components/product-form/product-form.component.ts
+++ product-version-maneger-frontend/src/app/components/product-form/product-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product-form',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.scss']
 })
 export class ProductFormComponent implements OnInit {
+  successMessage = 'Registrado exitosamente!';
+  showAlert = false;
+  form: FormGroup = this.formBuilder.group({
+    name: [null, [Validators.required]],
+    brand: [null, [Validators.required]],
+    price: [0, [Validators.required]],
+    seller: [null, [Validators.required]],
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  send(event: Event) {
+    event.preventDefault();
+    if (!this.form.valid) return this.form.markAsTouched();
+    const product: Product = this.form.value;
+    console.log(product);
+    this.DisplayAleter();
+    //this.productService.set(product).subscribe((product) => this.inserted.emit(product));
+    //this.form.reset();
+  }
+  private DisplayAleter(){
+    this.showAlert = true;
+    setTimeout(() => {
+      this.showAlert = false;
+    }, 1500);
+  }
+  public get nameField(): AbstractControl | null {
+    return this.form.get('name');
+  }
+
+  public get brandField(): AbstractControl | null {
+    return this.form.get('brand');
+  }
+
+  public get priceField(): AbstractControl | null {
+    return this.form.get('price');
+  }
+
+  public get sellerField(): AbstractControl | null {
+    return this.form.get('seller');
+  }
 }
