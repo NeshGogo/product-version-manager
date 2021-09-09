7ef9a37ede6afeedcaba2274d5d5de57b57e7cca
NeshGogo <rafaelaguero10@hotmail.com>
2021-09-09 16:09:57 -0400

    adding internationalization

--- product-version-maneger-frontend/src/app/services/product.service.ts
+++ product-version-maneger-frontend/src/app/services/product.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient, HttpResponse } from '@angular/common/http'
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
 import { Pagination } from '../models/pagination';
 import { ApplyVersion } from '../models/apply-version';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
   private readonly URL = `${environment.API}/products`;
 
   constructor(private http: HttpClient) {}
 
-  get(pagination?:Pagination): Observable<HttpResponse<Product[]>> {
-    return this.http.get<Product[]>(`${this.URL}?page=${pagination?.page}`, {observe: 'response'});
+  get(pagination?: Pagination): Observable<HttpResponse<Product[]>> {
+    return this.http.get<Product[]>(`${this.URL}?page=${pagination?.page}`, {
+      observe: 'response',
+    });
   }
 
-  geById(id:number): Observable<Product> {
+  geById(id: number): Observable<Product> {
     return this.http.get<Product>(`${this.URL}/${id}`);
   }
 
@@ -30,15 +32,21 @@ export class ProductService {
     return this.http.put<Product>(`${this.URL}/${id}`, product);
   }
 
-  filter(value: string, pagination?:Pagination): Observable<HttpResponse<Product[]>>{
-    return this.http.get<Product[]>(`${this.URL}/filter/${value}?page=${pagination?.page}`, {observe: 'response'});
+  filter(
+    value: string,
+    pagination?: Pagination
+  ): Observable<HttpResponse<Product[]>> {
+    return this.http.get<Product[]>(
+      `${this.URL}/filter/${value}?page=${pagination?.page}`,
+      { observe: 'response' }
+    );
   }
 
-  applyVersion(applyVersion: ApplyVersion){
+  applyVersion(applyVersion: ApplyVersion) {
     return this.http.post<Product>(`${this.URL}/applyVersion`, applyVersion);
   }
 
   delete(id: number) {
-    return this.http.delete(`${this.URL}/${id}`, {observe: 'response'});
+    return this.http.delete(`${this.URL}/${id}`, { observe: 'response' });
   }
 }
